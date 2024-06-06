6d72df624518ba6b5c86bd8a8ec84d6f746d6603
rababZein <rababelzen@yahoo.com>
2024-06-06 17:56:29 +0300

    rename var of current time

--- task2-tokens.php
+++ task2-tokens.php
@@ -3,9 +3,9 @@
  * 1-Initialize an empty $tokens array to store the token IDs and their expiry times.
  * 2- Track the maximum time $maxT seen in the commands.
  * 3- Loop through the $commands array:
- *      - If the command type is 0 (create token), add the token ID and its expiry time (current time $T plus $expiryLimit) to the $tokens array.
- *      - If the command type is 1 (reset token), check if the token ID exists in the $tokens array and if the current time $T is less than or equal to the existing expiry time. 
- *            If so, we update the expiry time to the current time $T plus $expiryLimit.
+ *      - If the command type is 0 (create token), add the token ID and its expiry time (current time $currentTimeplus $expiryLimit) to the $tokens array.
+ *      - If the command type is 1 (reset token), check if the token ID exists in the $tokens array and if the current time $currentTimeis less than or equal to the existing expiry time. 
+ *            If so, we update the expiry time to the current time $currentTimeplus $expiryLimit.
  * 4- After processing all the commands, count the number of tokens that are active at the maximum time $maxT. We do this by iterating through the $tokens array and counting the tokens whose expiry time is greater than $maxT.
  * 5- Finally, return the count of active tokens.
 
@@ -21,15 +21,15 @@ function numberOfTokens($expiryLimit, $commandsSize, $commands) {
     for ($i = 0; $i < $commandsSize; $i++) {
         $type = $commands[$i][0];
         $token_id = $commands[$i][1];
-        $T = $commands[$i][2];
+        $currentTime= $commands[$i][2];
 
-        $maxT = max($maxT, $T);
+        $maxT = max($maxT, $t);
 
         if ($type == 0) { // Create token
-            $tokens[$token_id] = $T + $expiryLimit;
+            $tokens[$token_id] = $currentTime+ $expiryLimit;
         } elseif ($type == 1) { // Reset token
-            if (isset($tokens[$token_id]) && $tokens[$token_id] >= $T) {
-                $tokens[$token_id] = $T + $expiryLimit;
+            if (isset($tokens[$token_id]) && $tokens[$token_id] >= $t) {
+                $tokens[$token_id] = $currentTime+ $expiryLimit;
             }
         }
     }
